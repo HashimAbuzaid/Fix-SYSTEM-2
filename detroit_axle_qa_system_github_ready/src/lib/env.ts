4f0ff46f94a3acd5c35bf6dbe341e14d5f04b779
HashimAbuzaid <habuzaid@detroitaxle.com>
2026-04-15 08:09:08 -0400

    Update env.ts

--- detroit_axle_qa_system_github_ready/src/lib/env.ts
+++ detroit_axle_qa_system_github_ready/src/lib/env.ts
@@ -9,6 +9,6 @@ function requireEnv(name: 'VITE_SUPABASE_URL' | 'VITE_SUPABASE_ANON_KEY') {
 }
 
 export const env = {
-  supabaseUrl: requireEnv('https://twhsnbyrsitqkosjxssh.supabase.co'),
-  supabaseAnonKey: requireEnv('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3aHNuYnlyc2l0cWtvc2p4c3NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMTEwMjksImV4cCI6MjA5MDg4NzAyOX0.oKSG-jg-fe5yKYDNrtOyi0r4tzsGiQsALd9gS1mcdp4'),
+  supabaseUrl: requireEnv('VITE_SUPABASE_URL'),
+  supabaseAnonKey: requireEnv('VITE_SUPABASE_ANON_KEY'),
 };
